755d65b7deacd25bf554614ca05346bc5b5cc696
Ziondreads1973 <pablo.higueras91@gmail.com>
2025-12-18 23:59:26 -0300

    sexto commit

--- src/app/profile/edit-profile/edit-profile.page.spec.ts
+++ src/app/profile/edit-profile/edit-profile.page.spec.ts
@@ -1,15 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage-angular';
+
 import { EditProfilePage } from './edit-profile.page';
 
 describe('EditProfilePage', () => {
   let component: EditProfilePage;
   let fixture: ComponentFixture<EditProfilePage>;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EditProfilePage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [EditProfilePage],
+      imports: [
+        IonicModule.forRoot(),
+        IonicStorageModule.forRoot(), // Provee Storage para StorageService
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(EditProfilePage);
+        component = fixture.componentInstance;
+        // No llamamos detectChanges() para no disparar ngOnInit/bindings innecesarios
+      });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- src/app/settings/settings.page.spec.ts
+++ src/app/settings/settings.page.spec.ts
@@ -1,11 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  IonicModule,
+  AlertController,
+  ToastController,
+  NavController,
+} from '@ionic/angular';
+
 import { SettingsPage } from './settings.page';
+import { AuthService } from '../core/auth.service';
+import { StorageService } from '../core/storage';
 
 describe('SettingsPage', () => {
   let component: SettingsPage;
   let fixture: ComponentFixture<SettingsPage>;
 
-  beforeEach(() => {
+  const authMock = {
+    getEmail: jasmine.createSpy('getEmail').and.returnValue('test@duoc.cl'),
+    logout: jasmine.createSpy('logout').and.resolveTo(true),
+  };
+
+  const storageServiceMock = {
+    get: jasmine.createSpy('get').and.resolveTo(null),
+    set: jasmine.createSpy('set').and.resolveTo(true),
+    remove: jasmine.createSpy('remove').and.resolveTo(true),
+    clear: jasmine.createSpy('clear').and.resolveTo(true),
+  };
+
+  const navMock = { back: jasmine.createSpy('back') };
+
+  const alertCtrlMock = {
+    create: jasmine
+      .createSpy('create')
+      .and.resolveTo({ present: jasmine.createSpy('present') }),
+  };
+
+  const toastCtrlMock = {
+    create: jasmine
+      .createSpy('create')
+      .and.resolveTo({ present: jasmine.createSpy('present') }),
+  };
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SettingsPage],
+      imports: [IonicModule.forRoot()],
+      providers: [
+        { provide: AuthService, useValue: authMock },
+        { provide: StorageService, useValue: storageServiceMock },
+        { provide: NavController, useValue: navMock },
+        { provide: AlertController, useValue: alertCtrlMock },
+        { provide: ToastController, useValue: toastCtrlMock },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SettingsPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
